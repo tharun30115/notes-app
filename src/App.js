c5e461c16aa290c141af564b8aa44bee4cc27209
Tharun <tharungowda30115@gmail.com>
2025-02-20 22:33:19 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import Sidebar from "./components/sidebar";
 
+ export const NotesContext = createContext(null);
+
 function App() {
+
+  const [notes,setNotes] = useState([])
+
+  const addNote = (theme) => {
+    setNotes([
+      {
+        id: Math.random().toString(36),
+        text: "",
+        theme,
+        timestamp: +new Date(),
+        editmode: true
+      },
+      ...notes
+    ])
+  }
+
+  const deleteNote = (noteId) => {
+    setNotes(
+      notes.filter(note => note.id !== noteId)
+    )
+  }
+
+  const value = {
+    notes
+  };
+
   return (
+    <NotesContext.Provider value ={value}>
     <div className="notes-app">
       <Sidebar/>
     </div>
+    </NotesContext.Provider>
   );
 }
 
--- src/components/notes-container/index.js
+++ src/components/notes-container/index.js
@@ -3,7 +3,7 @@ function NotesContainer(){
         <div className="notes-container">
             <h2>Notes</h2>
             <div children="notes-list">
-                
+                {NotesContainer.map(note => <div>Note</div>)}
             </div>
         </div>
     )
